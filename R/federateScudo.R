a04afd315eee26d68df43a30aa822efc2129a5d8
Riccardo-Cpt <riccardo.9giuliani.6@gmail.com>
2021-07-15 15:21:39 +0200

    message

--- R/federateScudo.R
+++ R/federateScudo.R
@@ -144,7 +144,7 @@ federateScudo <- function(loginFD, logins, queryvar, querytab, nTop=10, nBott=10
 
     return(result)
   }
-   
+  return(res$server3) 
   to_plot = scudoNetwork(res$server3, 0.2)
   return(to_plot)
 }
