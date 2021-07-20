5287cacaebcef86c1dd9f457138ebdf7d849b4d0
Riccardo-Cpt <riccardo.9giuliani.6@gmail.com>
2021-07-20 10:02:07 +0200

    message

--- R/federateScudo.R
+++ R/federateScudo.R
@@ -212,7 +212,7 @@ federateSSCPweight <- function(loginFD, logins, querytab, queryvar, TOL = 1e-10)
     nNode <- length(opals)
 
     datashield.assign(opals, "rawData", querytable, variables=queryvariables, async=T)
-    datashield.assign(opals, "indexMatrix", as.symbol('dsRank(cent)'), async=T)
+    datashield.assign(opals, "indexMatrix", as.symbol('dsRank(rawData)'), async=T)
     datashield.assign(opals, "weightMatrix",as.symbol("computeWeights(rawData, indexMatrix)"), async = T)
     datashield.assign(opals, "centeredData", as.symbol('center(computeWeights)'), async=T)
     datashield.assign(opals, "crossProdSelf", as.symbol('crossProd(centeredData)'), async=T)
