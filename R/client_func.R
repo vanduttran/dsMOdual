6a236b7d9df6b72440738e05f07c15e83cb58c1f
Thuong Tran <thuong.tran@sib.swiss>
2022-02-09 16:40:11 +0100

    v0.1.45

--- R/client_func.R
+++ R/client_func.R
@@ -1,27 +1,3 @@
-#' #' @title Encode function  argument
-#' #' @description Serialize to JSON, then encode base64,
-#' #'  then replace '+', '/' and '=' in the result in order to play nicely with the opal entry.
-#' #'  Used to encode non-scalar function arguments prior to sending to the opal server.
-#' #'  There's a corresponding function in the server package called .decode.arg.
-#' #'  See \code{dsSwissKnifeClient:::.encode.arg}.
-#' #' @param some.object the object to be encoded
-#' #' @return encoded text with offending characters replaced by strings
-#' #' @keywords internal
-#' .encode.arg <- function(some.object, serialize.it = TRUE){
-#'     if(serialize.it){
-#'         encoded <- paste0(RCurl::base64Encode(jsonlite::serializeJSON(some.object)), 'serialized')
-#'     } else {
-#'         encoded <- RCurl::base64Encode(jsonlite::toJSON(some.object, null = 'null'))
-#'     }
-#'     # go fishing for '+', '/' and '=', opal rejects them :
-#'     my.dictionary <- c('\\/' = '-slash-', '\\+' = '-plus-', '\\=' = '-equals-')
-#'     sapply(names(my.dictionary), function(x){
-#'         encoded[1] <<- gsub(x, my.dictionary[x], encoded[1])
-#'     })
-#'     return(paste0(encoded[1],'base64'))
-#' }
-
-
 #' @title Garbage collection
 #' @description Call gc on the federated server
 #' @export
